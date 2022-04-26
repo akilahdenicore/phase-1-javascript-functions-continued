cc803c3f4d6714e463ab2779171315ace7cf8cff
Akilah Snowden <akilahdsnowden@gmail.com>
2022-04-26 15:56:41 -0400

    commit tree 6462ec5c6e898cae0847dcc99e7396196e9bbe60

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // code your solution here
+//function declaration of saturdayFun, returns a string -> 'This Saturday I want to ...!'
+//default should equal 'roller-skate
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+
+function wrapAdjective(signal = '*') {
+    const anonFun = function(adjective = 'special'){
+    return `You are ${signal}${adjective}${signal}!`
+    }
+    return anonFun
+} */
